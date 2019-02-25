c471ed641bf6363be8589b5fa6e0f8414bbe9f83
Althedev <casaatci@gmail.com>
2019-02-25 16:45:38 +0300

    add edit update functionality and partial

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   resources :articles
 
-  get "signup", to: 'users#new'
-  post 'users', to: 'users#create'
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
